f374555fbb58483ba7b8f3e57bc5453ba22df6f4
jambox <eli.s.silverman@gmail.com>
2014-11-17 11:12:32 -0600

    Upgrade: allow simple titling mechanism for each Layout. Requires a 'section_title' ACF, or equivalent field for users to enter a label.

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -1,6 +1,16 @@
 jQuery(document).ready(function( $ ) {
 
 	$('.layout .layout').attr('data-toggle', 'closed');
+
+
+	$('.layout .layout:not(.acf-clone)').each( function() {
+		var $section_title_el = $(this).find('[data-name="section_title"]');
+
+		if( $section_title_el.length > 0 ) {
+			var curr_text = $(this).find('.acf-fc-layout-handle').text();
+			$(this).find('.acf-fc-layout-handle').html( '<span>' + curr_text + '– <strong>' + $section_title_el.find('.acf-input input').val() + '</strong></span>' );
+		}
+	});
 	
 
 	$( ".layout .layout .acf-fc-layout-handle" ).live( "click", function() {
